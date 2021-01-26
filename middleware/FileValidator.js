a0e87bc40817e6fd6e3409eb22fae2b8036d92bb
root <root@admin.sikota.com>
2021-01-26 17:21:51 +0700

    fix bug

--- middleware/FileValidator.js
+++ middleware/FileValidator.js
@@ -1,3 +1,4 @@
+
 class FileValidator {
     constructor() {
     }
@@ -6,10 +7,10 @@ class FileValidator {
         const allowFormat = [
             'image/jpg', 'image/jpeg', 'application/pdf', 'application/rtf', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
         ]; 
-        if (allowFormat.indexOf(format).toString() == "-1") return res.status(403).json({icon:"error",message:"File tidak di perbolehkan",titile:"Gagal",status:403,log:LogRequest.logFile(req.files.doc.name, 'Format File is Forbidden')});
+        if (allowFormat.indexOf(format).toString() == "-1") return res.status(403).json({icon:"error",message:"File tidak di perbolehkan",titile:"Gagal",status:403});
 
         const allowSize = 3410285; 
-        if (allowFormat.indexOf(req.files.doc.mimetype).toString() == '-1') res.status(403).json({status: 403,message: 'Format File is Forbidden',log: LogRequest.logFile(req.files.doc.name, 'Format File is Forbidden')});
+        if (allowSize.indexOf(req.files.doc.mimetype).toString() == '-1') res.status(403).json({status: 403,message: 'Format File is Forbidden'});
 
         next()
     }
